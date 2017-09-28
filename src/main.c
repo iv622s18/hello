dc67237469a858f91585650194e133721086a0ec
ChoiJulee <sheriodickenz@gmail.com>
2017-09-28 14:11:38 +0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -5,28 +5,28 @@
 
 int main(){
 	float a, b, c, d, x1, x2;
-	printf("%s", "Ââåäèòå a\n");
+	printf("%s", "Коэффициент a\n");
 	scanf("%f", &a);
-	printf("%s", "Ââåäèòå b\n");
+	printf("%s", "Коэффициент b\n");
 	scanf("%f", &b);
-	printf("%s", "Ââåäèòå c\n");
+	printf("%s", "Коэффициент c\n");
 	scanf("%f", &c);
 
 	d = D(a, b, c);
 
 	if (d < 0)
 	{
-		printf("Êîìïëåêñíûå êîðíè\n");
+		printf("Корней нет\n");
 	}
 	else if (d == 0)
 	{
-		printf("Îäèí êîðåíü\n");
+		printf("Один корень\n");
 		x1 = X(d, a, b, 1);
 		printf("\n%f", x1);
 	}
 	else
 	{
-		printf("Äâà êîðíÿ\n");
+		printf("Два корня\n");
 		x1 = X(d, a, b, 1);
 		x2 = X(d, a, b, -1);
 		printf("\n%f", x1);
