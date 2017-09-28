5ef770fe10b56e74dcb04dc65fce070ebf59a736
ChoiJulee <sheriodickenz@gmail.com>
2017-09-28 19:47:26 +0700

    Update validation_test.c

--- test/validation_test.c
+++ test/validation_test.c
@@ -6,8 +6,8 @@
 CTEST(check_input_data, check)
 {
 	const float a = 0;
-	const float b = 1;
-	const float c = 3;
+	const float b = 0;
+	const float c = 0;
 	const float result = check(a, b, c);
 	const float expected = -1;
 	ASSERT_DBL_NEAR(expected, result);
